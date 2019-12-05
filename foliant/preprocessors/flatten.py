cf72afb0e3e592f67c2ce69693adc7d432434cdc
Vanderhoof <ddddsa@gmail.com>
2019-12-05 18:28:41 +0300

    Feature/local links (#5)
    * add processing local links
    
    * better variable names

--- foliant/preprocessors/flatten.py
+++ foliant/preprocessors/flatten.py
@@ -1,9 +1,12 @@
+import re
+
 from pathlib import Path
 from typing import List, Dict
-Chapter = str or Dict[str, str] or Dict[str, List['Chapter']]
 
 from foliant.preprocessors.base import BasePreprocessor
 from foliant.preprocessors import includes
+
+Chapter = str or Dict[str, str] or Dict[str, List['Chapter']]
 
 
 def flatten(chapters: List[Chapter], working_dir: Path, buffer=[], heading_level=1) -> List[str]:
@@ -25,7 +28,7 @@ def flatten(chapters: List[Chapter], working_dir: Path, buffer=[], heading_level
                 )
 
             elif isinstance(value, list):
-                flatten(value, working_dir, buffer, heading_level+1)
+                flatten(value, working_dir, buffer, heading_level + 1)
 
     return buffer
 
@@ -41,6 +44,32 @@ class Preprocessor(BasePreprocessor):
 
         self.logger = self.logger.getChild('flatten')
         self.logger.debug(f'Preprocessor inited: {self.__dict__}')
+
+    def _process_local_links(self, source: str) -> str:
+        '''
+        Cut out path from local links, leaving only the anchor.
+
+        Since we are flattening all chapters into one file, all local links
+        (links to other chapters or local md files) will stop working. We assume
+        that all content which is referenced by local links was flattened into
+        the current document, which means that we can remove path from all the
+        local links and leave only the anchor.
+
+        This doesn't cover the case with dublicate anchors on the flattened page.
+
+        :param source: source file where the links should be processed.
+        :returns: processed source with paths cut out from all local links.
+        '''
+        def _fix_links(match) -> str:
+            if match.group('path').startswith('http'):
+                # external links are left unchanged
+                return match.group(0)
+
+            fixed_link = f'[{match.group("caption")}]({match.group("anchor")})'
+            return fixed_link
+
+        pattern = re.compile(r'\[(?P<caption>.+?)\]\((?P<path>.+)(?P<anchor>#.+)\)')
+        return pattern.sub(_fix_links, source)
 
     def apply(self):
         self.logger.debug('Applying preprocessor')
@@ -60,6 +89,8 @@ class Preprocessor(BasePreprocessor):
             self.logger,
             {'recursive': False}
         ).process_includes(flat_src_file_path, flat_src)
+
+        flat_src = self._process_local_links(flat_src)
 
         if not self.options['keep_sources']:
             for markdown_file in self.working_dir.rglob('*.md'):
